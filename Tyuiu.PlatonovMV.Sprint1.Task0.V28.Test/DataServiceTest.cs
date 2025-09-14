e49e2c78feaa996aa50dda5315e9d4521bbfec98
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-09-14 15:24:16 +0500

    Add project files.

---
+++ Tyuiu.PlatonovMV.Sprint1.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿
+using Tyuiu.PlatonovMV.Sprint1.Task0.V28.Lib;
+namespace Tyuiu.PlatonovMV.Sprint1.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
