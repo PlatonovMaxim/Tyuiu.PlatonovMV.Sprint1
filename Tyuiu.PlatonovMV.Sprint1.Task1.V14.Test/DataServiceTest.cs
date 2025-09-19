2dae974b588bfcf52a06968894c37aa2570d6a46
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-09-19 19:33:07 +0500

    Task1

---
+++ Tyuiu.PlatonovMV.Sprint1.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PlatonovMV.Sprint1.Task1.V14.Lib;
+namespace Tyuiu.PlatonovMV.Sprint1.Task1.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double a = 1.0;
+            double b = 2.0;
+            double c = 3.0;
+            var res = ds.Calculate(a, b ,c);
+            Assert.AreEqual(0.8667, res, 0.0001);
+            
+        }
+    }
+}
